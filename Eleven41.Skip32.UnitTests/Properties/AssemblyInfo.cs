5cf0bc7fd5ba8b8fd3e780a438c2ef02f5af3474
mwhouser <matt@eleven41.com>
2014-05-29 23:51:33 -0400

    Add unit tests

--- Eleven41.Skip32.UnitTests/Properties/AssemblyInfo.cs
+++ Eleven41.Skip32.UnitTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("Eleven41.Skip32.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Eleven41 Software Inc.")]
+[assembly: AssemblyProduct("Eleven41.Skip32.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © 2014, Eleven41 Software Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2b570cb2-c9c8-485b-9b48-ba7c2e7b9600")]
+[assembly: Guid("c34a3d01-f1f6-4d1d-af2e-c6ab9ac5817f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Eleven41.Skip32.UnitTests/Skip32CipherTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eleven41.Skip32.UnitTests
+{
+	[TestClass]
+	public class Skip32CipherTests
+	{
+		// This test takes a few minutes to execute
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void null_byte_array_key()
+		{
+			byte[] key = null;
+			Skip32Cipher cipher = new Skip32Cipher(key);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void incorrect_byte_array_key_length()
+		{
+			byte[] key = new byte[1];
+			Skip32Cipher cipher = new Skip32Cipher(key);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void null_hex_key()
+		{
+			string key = null;
+			Skip32Cipher cipher = new Skip32Cipher(key, Skip32CipherKeyFormat.HexString);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void null_base64_key()
+		{
+			string key = null;
+			Skip32Cipher cipher = new Skip32Cipher(key, Skip32CipherKeyFormat.Base64);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void empty_hex_key()
+		{
+			string key = "";
+			Skip32Cipher cipher = new Skip32Cipher(key, Skip32CipherKeyFormat.HexString);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void empty_base64_key()
+		{
+			string key = "";
+			Skip32Cipher cipher = new Skip32Cipher(key, Skip32CipherKeyFormat.Base64);
+		}
+
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void incorrect_hex_key_length()
+		{
+			string key = "abc";
+			Skip32Cipher cipher = new Skip32Cipher(key, Skip32CipherKeyFormat.HexString);
+		}
+
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void incorrect_base64_key_length()
+		{
+			string key = "abcd";
+			Skip32Cipher cipher = new Skip32Cipher(key, Skip32CipherKeyFormat.Base64);
+		}
+
+		[ExpectedException(typeof(FormatException))]
+		public void invalid_base64_key_length()
+		{
+			string key = "abcde";
+			Skip32Cipher cipher = new Skip32Cipher(key, Skip32CipherKeyFormat.Base64);
+		}
+
+		// This test takes a few minutes to execute
+		[TestMethod]
+		public void test_0()
+		{
+			Skip32Cipher cipher = new Skip32Cipher("1234567890abcdef0123", Skip32CipherKeyFormat.HexString);
+
+			int value0 = 0;
+			int value1 = cipher.Encrypt(value0);
+			int value2 = cipher.Decrypt(value1);
+
+			// Ensure that the encryption is reversible
+			Assert.AreEqual(value0, value2);
+
+			// Ensure the encryption is not an identity function
+			Assert.AreNotEqual(value0, value1);
+		}
+
+		[TestMethod]
+		public void test_1()
+		{
+			Skip32Cipher cipher = new Skip32Cipher("1234567890abcdef0123", Skip32CipherKeyFormat.HexString);
+
+			int value0 = 0;
+			int value1 = cipher.Encrypt(value0);
+			int value2 = cipher.Decrypt(value1);
+
+			// Ensure that the encryption is reversible
+			Assert.AreEqual(value0, value2);
+
+			// Ensure the encryption is not an identity function
+			Assert.AreNotEqual(value0, value1);
+		}
+
+		[TestMethod]
+		public void test_random()
+		{
+			Skip32Cipher cipher = new Skip32Cipher("1234567890abcdef0123", Skip32CipherKeyFormat.HexString);
+
+			Random r = new Random();
+			int value0 = r.Next();
+			int value1 = cipher.Encrypt(value0);
+			int value2 = cipher.Decrypt(value1);
+
+			// Ensure that the encryption is reversible
+			Assert.AreEqual(value0, value2);
+
+			// Ensure the encryption is not an identity function
+			Assert.AreNotEqual(value0, value1);
+		}
+
+		[TestMethod]
+		public void test_MinValue()
+		{
+			Skip32Cipher cipher = new Skip32Cipher("1234567890abcdef0123", Skip32CipherKeyFormat.HexString);
+
+			int value0 = int.MinValue;
+			int value1 = cipher.Encrypt(value0);
+			int value2 = cipher.Decrypt(value1);
+
+			// Ensure that the encryption is reversible
+			Assert.AreEqual(value0, value2);
+
+			// Ensure the encryption is not an identity function
+			Assert.AreNotEqual(value0, value1);
+		}
+
+		[TestMethod]
+		public void test_MaxValue()
+		{
+			Skip32Cipher cipher = new Skip32Cipher("1234567890abcdef0123", Skip32CipherKeyFormat.HexString);
+
+			int value0 = int.MaxValue;
+			int value1 = cipher.Encrypt(value0);
+			int value2 = cipher.Decrypt(value1);
+
+			// Ensure that the encryption is reversible
+			Assert.AreEqual(value0, value2);
+
+			// Ensure the encryption is not an identity function
+			Assert.AreNotEqual(value0, value1);
+		}
+	}
+}
